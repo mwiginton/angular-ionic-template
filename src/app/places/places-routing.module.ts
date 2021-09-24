66c5c8f0126d0b68e7a438305f21e054a2457bfa
Michelle Wiginton <mwiginton@nfsmith.com>
2021-09-24 11:57:22 -0500

    set up routing and tabs

--- src/app/places/places-routing.module.ts
+++ src/app/places/places-routing.module.ts
@@ -1,25 +1,63 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
 
 import { PlacesPage } from './places.page';
 
 const routes: Routes = [
   {
+    path: 'tabs',
+    component: PlacesPage,
+    children: [
+      {
+        path: 'search',
+        children: [
+          {
+            path: '',
+            loadChildren: () => import('./search/search.module').then(m => m.SearchPageModule)
+          },
+          {
+            path: ':placeId',
+            loadChildren: () => import('./search/place-detail/place-detail.module').then(m => m.PlaceDetailPageModule)
+          }
+        ]
+      },
+      {
+        path: 'offers',
+        children: [
+          {
+            path: '',
+            loadChildren: () => import('./offers/offers.module').then(m => m.OffersPageModule)
+          },
+          {
+            path: 'new',
+            loadChildren: () => import('./offers/new-offer/new-offer.module').then(m => m.NewOfferPageModule)
+          },
+          {
+            path: 'edit/:placeId',
+            loadChildren: () => import('./offers/edit-offer/edit-offer.module').then(m => m.EditOfferPageModule)
+          },
+          {
+            path: ':placeId',
+            loadChildren: () => import('./offers/offer-bookings/offer-bookings.module').then(m => m.OfferBookingsPageModule)
+          }
+        ]
+      },
+      {
+        path: '',
+        redirectTo: '/places/tabs/search',
+        pathMatch: 'full'
+      }
+    ]
+  },
+  {
     path: '',
-    component: PlacesPage
-  },
-  {
-    path: 'search',
-    loadChildren: () => import('./search/search.module').then( m => m.SearchPageModule)
-  },
-  {
-    path: 'offers',
-    loadChildren: () => import('./offers/offers.module').then( m => m.OffersPageModule)
+    redirectTo: '/places/tabs/search',
+    pathMatch: 'full'
   }
 ];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule],
+  exports: [RouterModule]
 })
 export class PlacesPageRoutingModule {}
