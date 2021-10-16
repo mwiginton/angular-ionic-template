942d6d45337193bc93317c3d181e7d57b8fa6dbf
Michelle Wiginton <michelle.wiginton00@gmail.com>
2021-10-16 13:33:32 -0500

    more dummy data

--- src/app/bookings/bookings.page.ts
+++ src/app/bookings/bookings.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { BookingsService } from './bookings.service';
+import { Booking } from './booking-model';
+import { IonItemSliding } from '@ionic/angular';
 
 @Component({
   selector: 'app-bookings',
@@ -6,10 +9,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./bookings.page.scss'],
 })
 export class BookingsPage implements OnInit {
+  loadedBookings: Booking[];
 
-  constructor() { }
+  constructor(private bookingsService: BookingsService) { }
 
   ngOnInit() {
+    this.loadedBookings = this.bookingsService.bookings;
+  }
+
+  onCancelBooking(offerId: string, slidingEl: IonItemSliding) {
+    slidingEl.close();
+
   }
 
 }
