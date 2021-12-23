c4931086c165d03e04a2f3ae9dc11607d3ffc246
Michelle Wiginton <michelle.wiginton00@gmail.com>
2021-12-23 14:55:05 -0600

    sample filter function

--- src/app/places/search/search.page.ts
+++ src/app/places/search/search.page.ts
@@ -3,6 +3,7 @@ import { SegmentChangeEventDetail } from '@ionic/core';
 import { Subscription } from 'rxjs';
 import { Place } from '../place.model';
 import { PlacesService } from '../places.service';
+import { AuthService } from '../../auth/auth.service';
 
 @Component({
   selector: 'app-search',
@@ -12,18 +13,30 @@ import { PlacesService } from '../places.service';
 export class SearchPage implements OnInit, OnDestroy {
 
   public loadedPlaces: Place[];
+  public relevantPlaces: Place[];
   private placesSub: Subscription;
 
-  constructor(private placesService: PlacesService) { }
+  constructor(
+    private placesService: PlacesService,
+    private authService: AuthService
+  ) { }
 
   ngOnInit() {
     this.placesSub = this.placesService.places.subscribe(places => {
       this.loadedPlaces = places;
+      this.relevantPlaces = this.loadedPlaces;
     });
   }
 
   onFilterUpdate(event: CustomEvent<SegmentChangeEventDetail>) {
     console.log(event.detail);
+    if (event.detail.value === 'all') {
+      this.relevantPlaces = this.loadedPlaces;
+    } else {
+      this.relevantPlaces = this.loadedPlaces.filter(place => place.userId !== this.authService.userId);
+      console.log("relevant places");
+      console.log(this.relevantPlaces);
+    }
   }
 
   ngOnDestroy() {
