eb7863240690221f5ca34862b81e780a3cca2fcb
Michelle Wiginton <michelle.wiginton00@gmail.com>
2022-01-01 11:58:44 -0600

    add http post request for create offer

--- src/app/places/places.service.ts
+++ src/app/places/places.service.ts
@@ -2,7 +2,8 @@ import { Injectable } from '@angular/core';
 import { BehaviorSubject } from 'rxjs';
 import { AuthService } from '../auth/auth.service';
 import { Place } from './place.model';
-import { take, map, tap, delay } from 'rxjs/operators';
+import { take, map, tap, delay, switchMap } from 'rxjs/operators';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
@@ -19,7 +20,10 @@ export class PlacesService {
     return this._places.asObservable();
   }
 
-  constructor(private authService: AuthService) { }
+  constructor(
+    private authService: AuthService,
+    private http: HttpClient
+  ) { }
 
   getPlace(id: any) {
     return this.places.pipe(
@@ -31,15 +35,33 @@ export class PlacesService {
   }
 
   addPlace(place: Place) {
-    place.id = Math.random().toString();
+    // place.id = Math.random().toString();
+    let generatedId: string;
+    place.id = null;
     place.userId = this.authService.userId;
 
-    return this.places.pipe(
+    return this.http
+    .post<{name: string}>('https://angular-ionic-template-default-rtdb.firebaseio.com/offered-places.json', { 
+      ...place
+    })
+    .pipe(
+      switchMap(data => {
+        generatedId = data.name;
+        return this.places;
+      }),
       take(1),
-      delay(1000), 
       tap(places => {
+        place.id = generatedId;
         this._places.next(places.concat(place));
-    }));
+      })
+    );
+
+    // return this.places.pipe(
+    //   take(1),
+    //   delay(1000), 
+    //   tap(places => {
+    //     this._places.next(places.concat(place));
+    // }));
   }
 
   updatePlace(place: any) {
